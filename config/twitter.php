b8bf0a64897a3a8cc5976113ea592ef3ee677c4e
miyazawayamato <bmiyazawayamato@gmail.com>
2021-03-17 21:39:57 +0900

    log削除

--- config/twitter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    
-    'api_key' => env('TWITTER_CLIENT_ID', ''),
-    'secret_key' => env('TWITTER_CLIENT_SECRET', ''),
-    'access_token' => env('TWITTER_CLIENT_ID_ACCESS_TOKEN', ''),
-    'access_token_secret' => env('TWITTER_CLIENT_ID_ACCESS_TOKEN_SECRET', ''),
-];
