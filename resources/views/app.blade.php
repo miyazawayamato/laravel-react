483b81f154a70bd3f6aaff8deb7746c2cbe33735
miyazawayamato <bmiyazawayamato@gmail.com>
2021-03-19 03:03:43 +0900

    タイトル

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'React導入テスト') }}</title>
+    <title>コロナウイルス・状況</title>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
